8e1d12b524fd007a256fdaa3aea5cb1985925e13
dcollis92 <davidphilipcollis@gmail.com>
2022-10-03 15:19:25 -0500

    init git

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,21 @@
+import "./Landing.scss";
+import "./assets/Landing-page-image.svg";
+
+function Landing() {
+  return (
+    <>
+      <div>Imagine if Snapchat had events.</div>
+      <div>
+        Easily host and share events with your friends across any social media.
+      </div>
+      <div>
+        <img src="./assets/Landing-page-image.svg" alt="Landing page" />
+      </div>
+      <div>
+        <button>Create my event</button>
+      </div>
+    </>
+  );
+}
+
+export default Landing;
